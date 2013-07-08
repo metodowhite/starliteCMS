2e9b9aad2f8b913eabc435a6065fb90426d7e6a0
Miguel Ángel Bueno <miguel@metodowhite.com>
2013-07-08 19:57:59 +0200

    New entity Event

--- assets/js/edit_artist.js
+++ assets/js/edit_artist.js
@@ -1,7 +1,18 @@
-
-
-
 $(initArtistList);
+
+function GetURLParameter(sParam)
+{
+    var sPageURL = window.location.search.substring(1);
+    var sURLVariables = sPageURL.split('&');
+    for (var i = 0; i < sURLVariables.length; i++) 
+    {
+        var sParameterName = sURLVariables[i].split('=');
+        if (sParameterName[0] == sParam) 
+        {
+            return sParameterName[1];
+        }
+    }
+}​
 
 function initArtistList(){
 
